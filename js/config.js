9343efceee3442738090b4ff2d0ee4a62b16032f
ImKal <69103102+Im-Kal@users.noreply.github.com>
2022-04-07 10:20:56 +0400

    a

--- js/config.js
+++ js/config.js
@@ -295,7 +295,7 @@ const CONFIG = {
   /**
    * The delimiter between the hours and minutes on the clock.
    */
-  clockDelimiter: ' ',
+  clockDelimiter: ':',
 
   /**
    * Show a twenty-four-hour clock instead of a twelve-hour clock with AM/PM.
